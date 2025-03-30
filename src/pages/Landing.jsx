cfd3de73f91e9f89920ff3cb02785373ea30ef9b
ParthNarkhede <parthnarkhede1120@gmail.com>
2025-03-30 18:42:46 +0530

    created the Landing page

---
+++ src/pages/Landing.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Navbar from '../components/student/NavBar'
+
+function Landing() {
+    return (
+        <div><Navbar /></div>
+    )
+}
+
+export default Landing
