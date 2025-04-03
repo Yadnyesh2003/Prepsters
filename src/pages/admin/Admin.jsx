e4eaf2041dcc6f3c28fbf1996a0cfb17c56da288
ParthNarkhede <parthnarkhede1120@gmail.com>
2025-04-03 18:27:41 +0530

    Added Logout button to these files

--- src/pages/admin/Admin.jsx
+++ src/pages/admin/Admin.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import { useAuth } from '../../context/AuthContext'
 
 const Admin = () => {
+  const { logoutUser } = useAuth()
   return (
     <div>
       <h1>Admin Outlet main page.</h1>
-      <Outlet/>
+      {/* <Outlet /> */}
+      <button onClick={logoutUser}>Logout</button>
     </div>
   )
 }
--- src/pages/student/Home.jsx
+++ src/pages/student/Home.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { useAuth } from '../../context/AuthContext'
 
 const Home = () => {
+  const { logoutUser } = useAuth()
   return (
-    <h1 className='mt-7 text-red-700'>Welcome to Home Page!</h1>
+    <div>
+      <h1 className='mt-7 text-red-700'>Welcome to Home Page!</h1>
+      <button onClick={logoutUser}>Logout</button>
+    </div>
+    
+
   )
 }
 
