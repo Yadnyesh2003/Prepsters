53d0716da87b715debb49b635ce0b58bcebe39b0
ParthNarkhede <parthnarkhede1120@gmail.com>
2025-04-08 23:15:07 +0530

    added a new functionality of loading

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import { LoaderProvider } from './context/ThemeContext'
 
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <LoaderProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </LoaderProvider>,
 )
