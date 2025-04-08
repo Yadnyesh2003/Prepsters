a0a87c20f54d0eb5074f8bb3a529f83f69f8c572
ParthNarkhede <parthnarkhede1120@gmail.com>
2025-04-08 23:18:23 +0530

    edited landing page to add new components

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Navbar from '../components/student/NavBar'
+import Hero from '../components/student/Hero'
+import Features from '../components/student/Features'
+import HowItWorks from '../components/student/HowItWorks'
+import ImpactSection from '../components/student/Impacts'
+import Footer from '../components/student/Footer'
+import Loader from '../components/student/Loading'
+import { useTheme } from '../context/ThemeContext'
+
 
 function Landing() {
+
+    const { loading } = useTheme();
+
+    if (loading) {
+        return (
+            <Loader />
+        );
+    }
     return (
-        <div><Navbar /></div>
+        <div><Navbar />
+            <Hero />
+            <Features />
+            <HowItWorks />
+            <ImpactSection />
+            <Footer />
+        </div>
+
     )
 }
 
