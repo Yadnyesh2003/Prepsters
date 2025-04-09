6ebc972afe90e7e2a791a9f0966166b656907ea2
ParthNarkhede <parthnarkhede1120@gmail.com>
2025-04-10 01:54:35 +0530

    added loading checking to the protectedroute such that is avoids the redirection to '/' after nested routes calling

--- src/middleware/ProtectedRoute.jsx
+++ src/middleware/ProtectedRoute.jsx
@@ -1,17 +1,19 @@
 // components/ProtectedRoute.jsx
 import React from "react";
 import { Navigate } from "react-router-dom";
-import { useAuth } from "../context/AuthContext"; // update path if different
+import { useAuth } from "../context/AuthContext";
+import Loader from "../components/student/Loading";
 
 const ProtectedRoute = ({ children, allowedRole }) => {
-    const { isAuth, role } = useAuth();
+    const { isAuth, role, loading } = useAuth();
+    if (loading) return <Loader />;
 
     if (!isAuth) {
         return <Navigate to="/" replace />;
     }
 
     if (allowedRole && role !== allowedRole) {
-        return <Navigate to="/unauthorized" replace />;
+        return <Navigate to="/ghost/unauthorized" replace />;
     }
 
     return children;
