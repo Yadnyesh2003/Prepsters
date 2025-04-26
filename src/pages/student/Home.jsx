501ab7b9b0d35b8f2967c57553f82f946d9edb1d
TheThirdTierEngineers0620 <thethirdtierengineers@gmail.com>
2025-04-26 23:04:18 +0530

    Update Home.jsx

--- src/pages/student/Home.jsx
+++ src/pages/student/Home.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 const Home = () => {
   return (
+    <div>
     <h1 className='mt-7 text-red-700'>Welcome to Home Page!</h1>
     <h1 className='mt-7 text-red-700'>Welcome to Home Page! This is added by launcher</h1>
     <h1 className='mt-7 text-red-700'>Welcome to Home Page! This is added by tte</h1>
+    </div>  
   )
 }
 
